32a1799b4353a47c1614e57c94dd528b75d758a9
Sinéad Cheung <sineadccheung@gmail.com>
2016-08-14 15:22:43 -0500

    added mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,7 +5,7 @@ class Kele
 
   base_uri "https://www.bloc.io/api/v1/"
 
-def initialize(email, password)
+  def initialize(email, password)
     response = self.class.post(api_url("sessions"), body: {"email": email, "password": password})
     raise InvalidStudentCodeError.new() if response.code == 401
     @auth_token = response["auth_token"]
@@ -15,3 +15,10 @@ def initialize(email, password)
     response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
     body = JSON.parse(response.body)
   end
+  
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    body = JSON.parse(response.body)
+  end
+  
+end
