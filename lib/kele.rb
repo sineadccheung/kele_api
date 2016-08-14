513cec28f8c5ed22e38d0835dca7d7d3ffcde4b4
Sinéad Cheung <sineadccheung@gmail.com>
2016-08-14 15:19:02 -0500

    Update kele.rb

---
+++ lib/kele.rb
@@ -0,0 +1,17 @@
+require 'httparty'
+
+class Kele
+  include HTTParty
+
+  base_uri "https://www.bloc.io/api/v1/"
+
+def initialize(email, password)
+    response = self.class.post(api_url("sessions"), body: {"email": email, "password": password})
+    raise InvalidStudentCodeError.new() if response.code == 401
+    @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
+    body = JSON.parse(response.body)
+  end
