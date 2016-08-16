6857118e733e272572607bd2823d0c1dc1ac396d
Sinéad Cheung <sineadccheung@gmail.com>
2016-08-15 22:04:11 -0400

    Update kele.rb

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,33 +1,49 @@
 require 'httparty'
 require 'json'
-require 'kele/roadmap'
+require './lib/roadmap'
 
 class Kele
   include HTTParty
   include Roadmap
-
-  base_uri "https://www.bloc.io/api/v1/"
+  base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-    response = self.class.post(api_url("sessions"), body: {"email": email, "password": password})
-    raise InvalidStudentCodeError.new() if response.code == 401
-    @auth_token = response["auth_token"]
+    post_response = self.class.post('/sessions',
+    body: { email: email, password: password } )
+    @auth_token = post_response['auth_token']
+    if @auth_token.nil?
+      puts "Sorry, invalid credentials."
+    end
   end
 
   def get_me
-    response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
-    body = JSON.parse(response.body)
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    @current_user = JSON.parse(response.body)
   end
-  
+
   def get_mentor_availability(mentor_id)
-    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
-    body = JSON.parse(response.body)
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token })
+    @mentor_availability = JSON.parse(response.body)
   end
-  
-  private
-  
-    def api_url(endpoint)
-      "https://www.bloc.io/api/v1/#{endpoint}"
+
+  def get_messages(page = 0)
+    if page > 0
+      request_url = "/message_threads?page=#{page}"
+    else
+      request_url = "/message_threads"
     end
-  
+    response = self.class.get(request_url, headers: { "authorization" => @auth_token })
+    @messages = JSON.parse(response.body)
+  end
+
+  def create_message(user_id, recipient_id, subject, body)
+    self.class.post("/messages",
+    body: { user_id: user_id, recipient_id: recipient_id, subject: subject, stripped: body })
+  end
+
+  def create_submission (checkpoint_id, comment)
+    @enrollment_id = self.get_me['id']
+    self.class.post("/checkpoint_submissions",
+    body: { checkpoint_id: checkpoint_id, comment: comment, enrollment_id: @enrollment_id })
+  end
 end
